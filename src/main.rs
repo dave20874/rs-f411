8cfeead3e170e9aab552b543c5b8f11dd538620c
David Wheeler <dwheeler20874@gmail.com>
2023-08-04 17:25:35 -0400

    I got talky task working and button read is in a task, not main().
    The type ExtiInput<'static, PC13> had to be hard coded into the button
    reader task.  It seems that could be more generic, somehow.

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,12 @@ use embassy_stm32::gpio::{Level, Output, Speed, AnyPin, Input, Pull};
 use embassy_stm32::exti::ExtiInput;
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
-use heapless::Arc;
-use event_listener::Event;
-
+use embassy_sync::signal::Signal;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_stm32::peripherals::PC13;
 
 static PERIOD_MS: Mutex<ThreadModeRawMutex, u64> = Mutex::new(200);
+static TICKLE: Signal<CriticalSectionRawMutex, ()> = Signal::new();
 
 #[embassy_executor::task]
 async fn blinky(pin: AnyPin) {
@@ -39,41 +40,15 @@ async fn blinky(pin: AnyPin) {
 }
 
 #[embassy_executor::task]
-async fn talky(event: Event) {
-    let listener = event.listen();
+async fn talky() {
+    let mut fast: bool = false;
 
     loop {
-        listener.await;
+        TICKLE.wait().await;
+        // Timer::after(Duration::from_millis(1000)).await;
 
         info!("That tickles!");
-    }
-}
 
-
-
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
-
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
-
-    let mut fast: bool = false;
-    let event = Event::new();
-
-    spawner.spawn(talky(event));
-    spawner.spawn(blinky(p.PA5.into())).unwrap();
-
-    let button = Input::new(p.PC13, Pull::Up);
-    let mut ibutton = ExtiInput::new(button, p.EXTI13);
-    loop {
-        // Asynchronously wait for GPIO events, allowing other tasks
-        // to run, or the core to sleep.
-        ibutton.wait_for_falling_edge().await;
-
-        // signal another task
-        event.notify();
-
-        info!("Button pressed!");
         fast = !fast;
         {
             let mut period_ms = PERIOD_MS.lock().await;
@@ -88,7 +63,40 @@ async fn main(spawner: Spawner) {
             }
             info!("Delay: {}", *period_ms);
         }
-        
+    }
+}
+
+// I don't like that PC13 had to be hard coded here.  Is there a more generic type that could be used?
+#[embassy_executor::task]
+async fn button_mon(mut button: ExtiInput<'static, PC13>) {
+
+    loop {
+        let x = button.wait_for_falling_edge();
+        x.await;
+
+        info!("The button!");
+        TICKLE.signal(());  // send the tickle signal
+         
         Timer::after(Duration::from_millis(10)).await;  // debounce
+    }
+}
+
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+
+    let p = embassy_stm32::init(Default::default());
+    
+    let button = Input::new(p.PC13, Pull::Up);
+    let ibutton = ExtiInput::new(button, p.EXTI13);
+
+    info!("Hello World!");
+
+    spawner.spawn(talky()).unwrap();
+    spawner.spawn(blinky(p.PA5.into())).unwrap();
+    spawner.spawn(button_mon(ibutton)).unwrap();
+
+    loop {
+        Timer::after(Duration::from_millis(1000)).await;  // debounce
     }
 }
